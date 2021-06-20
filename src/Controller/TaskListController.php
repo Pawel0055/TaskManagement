2e638ac1755ea89a79f6d82c00fbfcf50bec68d5
Pawel <pawelr608@gmail.com>
2021-06-20 19:53:42 +0200

    Add new controller for viewing list tasks

--- src/Controller/TaskListController.php
+++ src/Controller/TaskListController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TaskRepository;
+use App\Entity\Task;
+use Doctrine\ORM\EntityManagerInterface;
 
 class TaskListController extends AbstractController
 {
@@ -13,8 +16,11 @@ class TaskListController extends AbstractController
      */
     public function index(): Response
     {
+        $repository = $this->getDoctrine()->getRepository(Task::class);
+        $tasks = $repository->findAll();
         return $this->render('task_list/index.html.twig', [
             'controller_name' => 'TaskListController',
+            'tasks' => $tasks
         ]);
     }
 }
